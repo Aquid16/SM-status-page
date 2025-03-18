ea27cfdaa91333416e2486619f4eed3c70b4b409
Aquid16 <aqu2id@gmail.com>
2025-03-18 11:38:21 +0200

    Removed IAM Role module

--- terrform/modules/access_entries/variables.tf
+++ terrform/modules/access_entries/variables.tf
@@ -1,9 +1,9 @@
-variable "cluster_name" {
-  description = "Name of the EKS cluster"
-  type        = string
-}
+#variable "cluster_name" {
+#  description = "Name of the EKS cluster"
+#  type        = string
+#}
 
-variable "principal_arns" {
-  description = "List of IAM principal ARNs to grant access"
-  type        = list(string)
-}
+#variable "principal_arns" {
+#  description = "List of IAM principal ARNs to grant access"
+#  type        = list(string)
+#}
--- terrform/modules/iam_roles/outputs.tf
+++
@@ -1,9 +0,0 @@
-output "cluster_role_arn" {
-  description = "ARN of the IAM role for the EKS cluster"
-  value       = aws_iam_role.eks_cluster_role.arn
-}
-
-output "node_group_role_arn" {
-  description = "ARN of the IAM role for the EKS node group"
-  value       = aws_iam_role.eks_node_group_role.arn
-}
