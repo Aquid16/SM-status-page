257ccc30cea9282a3487f61e3fa2e045f0f96ed9
Meital <121434673+MeitalTal@users.noreply.github.com>
2025-03-24 16:10:09 +0200

    Update tests.py

--- docker/statuspage/status-page/statuspage/subscribers/tests.py
+++ docker/statuspage/status-page/statuspage/subscribers/tests.py
@@ -1,3 +1,23 @@
 from django.test import TestCase
+from statuspage.subscribers.models import Subscriber
+from django.urls import reverse
 
-# Create your tests here.
+class SubscriberTests(TestCase):
+
+    def setUp(self):
+        self.subscriber_data = {'email': 'test@example.com'}
+        self.subscriber = Subscriber.objects.create(**self.subscriber_data)
+
+    def test_create_subscriber(self):
+        subscriber = Subscriber.objects.create(email="newsubscriber@example.com")
+        self.assertEqual(subscriber.email, "newsubscriber@example.com")
+
+    def test_subscriber_api(self):
+        url = reverse('subscribers:subscriber_list')
+        response = self.client.get(url)
+        self.assertEqual(response.status_code, 200)
+
+    def test_subscriber_detail(self):
+        url = reverse('subscribers:subscriber', kwargs={'pk': self.subscriber.pk})
+        response = self.client.get(url)
+        self.assertEqual(response.status_code, 200)
