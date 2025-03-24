204eb4547893d2a834e0a6fcecab7234284ae319
Meital <121434673+MeitalTal@users.noreply.github.com>
2025-03-24 16:11:21 +0200

    Update tests.py

--- docker/statuspage/status-page/statuspage/queuing/tests.py
+++ docker/statuspage/status-page/statuspage/queuing/tests.py
@@ -1,3 +1,23 @@
 from django.test import TestCase
+from statuspage.queuing.models import Queue
+from django.urls import reverse
 
-# Create your tests here.
+class QueueTests(TestCase):
+
+    def setUp(self):
+        self.queue_data = {'name': 'Processing Queue'}
+        self.queue = Queue.objects.create(**self.queue_data)
+
+    def test_create_queue(self):
+        queue = Queue.objects.create(name="New Queue")
+        self.assertEqual(queue.name, "New Queue")
+
+    def test_queue_api(self):
+        url = reverse('queuing:queue_list')
+        response = self.client.get(url)
+        self.assertEqual(response.status_code, 200)
+
+    def test_queue_detail(self):
+        url = reverse('queuing:queue', kwargs={'pk': self.queue.pk})
+        response = self.client.get(url)
+        self.assertEqual(response.status_code, 200)
