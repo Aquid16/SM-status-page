8db3f926ca2032323d5d63a55195477235e22a67
Sharon Sinelnikov <54771966+Aquid16@users.noreply.github.com>
2025-03-25 11:47:51 +0200

    Update variables.tf

--- terraform/statuspage_pr/variables.tf
+++ terraform/statuspage_pr/variables.tf
@@ -1,14 +1,3 @@
-variable "AWS_ACCESS_KEY_ID" {
-  description = "AWS access key ID"
-  type        = string
-}
-
-variable "AWS_SECRET_ACCESS_KEY" {
-  description = "AWS secret access key"
-  type        = string
-}
-
-variable "AWS_DEFAULT_REGION" {
-  description = "The AWS region to operate in"
-  type        = string
-}
+variable "aws_access_key" {}
+variable "aws_secret_key" {}
+variable "aws_region" {}
