fac32df75fdab43a7197583ccc0b3a57035af7c2
Sharon Sinelnikov <54771966+Aquid16@users.noreply.github.com>
2025-03-25 11:47:07 +0200

    Update provider.tf

--- terraform/statuspage_pr/provider.tf
+++ terraform/statuspage_pr/provider.tf
@@ -1,5 +1,5 @@
 provider "aws" {
-  region     = var.AWS_DEFAULT_REGION
-  access_key = var.AWS_ACCESS_KEY_ID
-  secret_key = var.AWS_SECRET_ACCESS_KEY
+  access_key = var.aws_access_key
+  secret_key = var.aws_secret_key
+  region     = var.aws_region
 }
