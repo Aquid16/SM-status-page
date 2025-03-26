29797c7640b6c2bfd1931a0663a04b1da7042e55
Sharon Sinelnikov <54771966+Aquid16@users.noreply.github.com>
2025-03-26 13:12:36 +0200

    Update output.tf

--- terraform/statuspage_pr/output.tf
+++ terraform/statuspage_pr/output.tf
@@ -5,3 +5,7 @@ output "rds_endpoint" {
 output "efs_filesystem_id" {
   value = aws_efs_file_system.statuspage_efs.id
 }
+
+output "bastion_public_ip" {
+  value = aws_instance.bastion.public_ip  # Adjust based on your bastion resource name
+}
