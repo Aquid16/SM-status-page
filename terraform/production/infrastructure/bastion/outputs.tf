e9ea9b9cdf29c37ef8b3e7f0014eaedf097d6c64
Meital <121434673+MeitalTal@users.noreply.github.com>
2025-03-29 16:27:29 +0300

    Update outputs.tf

--- terraform/production/infrastructure/bastion/outputs.tf
+++ terraform/production/infrastructure/bastion/outputs.tf
@@ -1,8 +1,3 @@
-output "bastion_sg_id" {
-  description = "ID of the Bastion Host security group"
-  value       = aws_security_group.bastion_sg.id
-}
-
 output "bastion_public_ip" {
   description = "Public IP of the Bastion Host"
   value       = aws_instance.bastion.public_ip
